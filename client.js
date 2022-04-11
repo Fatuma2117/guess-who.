dcae140e251720af7bb9c14c339e425719c1af47
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-04-11 16:27:35 -0500

    added a random number function

--- client.js
+++ client.js
@@ -1,1 +1,14 @@
 console.log('Here are all the available people:', people);
+$(document).ready(readyNow);
+
+function readyNow() {
+    for (let user of people) {
+       $('body').append(`<img src="https://github.com/${user.githubUsername}.png?size=250" alt="Profile image of Chris">`)
+    }
+}
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
+console.log(randomNumber(0 ,6));
+console.log(people[randomNumber(0,6)].name);
